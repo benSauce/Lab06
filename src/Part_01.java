b2949a36c90a185840838ec3f44318be627f7690
BenSauce <benjaminsaucedo7@gmail.com>
2024-03-06 19:56:31 -0800

    Part 1 complete, updated to track string

--- src/Part_01.java
+++ src/Part_01.java
@@ -7,12 +7,14 @@ public class Part_01 {
         System.out.print("Enter a String: ");
         //Store string
         String strInput = input.nextLine();
+        strInput = strInput.toLowerCase();
         //Create variables for counting
         int subStrCount = 0;
         int maxCount = 0;
+        String maxConsecutive = "";
 
         //check string length to begin counting
-        if ((strInput.length()) > 0) {
+        if (!strInput.isEmpty()) {
             String subStr = String.valueOf(strInput.charAt(0));;
             subStrCount = 1;
             maxCount = 1;
@@ -22,19 +24,21 @@ public class Part_01 {
                 if (strInput.charAt(i) < strInput.charAt(i + 1)) {
                     subStrCount++;
                     subStr += String.valueOf(strInput.charAt(i + 1));
+
                     if (subStrCount > maxCount) {
                         maxCount = subStrCount;
+                        maxConsecutive = subStr;
                     } //end if
                 } //end if
                 else {
                     //reset count
                     subStrCount = 1;
-                    subStr = "";
+                    subStr = String.valueOf(strInput.charAt(i + 1));
                 }
                 //Print result
             } // end for
             System.out.println("The maximum consecutive increasingly ordered substring:");
-            System.out.println(subStr + " | count: " + maxCount);
+            System.out.println(maxConsecutive + " | count: " + maxCount);
         } else {
             System.out.println("You must enter a String of at least length 1.");
         }
